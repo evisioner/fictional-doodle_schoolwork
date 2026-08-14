878fc05d2164cf9d2699f9427107a414c63b287c
evisioner <sugomadic116@gmail.com>
2026-08-14 19:53:36 +0700

    2nd

---
+++ 7-8/7-8/10.py
@@ -0,0 +1,36 @@
+import string
+import random
+
+digits=string.digits
+letters=string.ascii_letters
+lower=string.ascii_lowercase
+upper=string.ascii_uppercase
+special=string.punctuation
+store=digits+letters+special
+
+password=[]
+while True:
+    limit=int(input())
+    if limit < 4:
+        print('pls try again')
+    else:
+        break
+
+password.append(random.choice(lower))
+password.append(random.choice(upper))
+password.append(random.choice(digits))
+password.append(random.choice(special))
+
+for i in range((limit-4)):
+    password.append(random.choice(store))
+
+random.shuffle(password)
+
+print('your password is:', ''.join(password))
+
+if len(password) < 8:
+    print('this password is weak')
+elif len(password) >= 8 and len(password) < 12:
+    print('this password is medium')
+else:
+    print('this password is strong')
---
+++ 7-8/7-8/9.py
@@ -0,0 +1,35 @@
+import string
+import random
+
+digits=string.digits
+letters=string.ascii_letters
+lower=string.ascii_lowercase
+upper=string.ascii_uppercase
+special=string.punctuation
+omit='lioO0I1'
+store=digits+letters+special
+
+for char in omit:
+    store=store.replace(char, '')
+
+password=[]
+limit=int(input())
+
+password.append(random.choice(lower))
+password.append(random.choice(upper))
+password.append(random.choice(digits))
+password.append(random.choice(special))
+
+for i in range((limit-4)):
+    password.append(random.choice(store))
+
+random.shuffle(password)
+
+print(''.join(password))
+
+if len(password) < 8:
+    print('this password is weak')
+elif len(password) >= 8 and len(password) < 12:
+    print('this password is medium')
+else:
+    print('this password is strong')
